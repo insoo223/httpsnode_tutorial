336fa7a6b4e8378282884ba1d9d29607b873f2f5
Insoo Kim <insoo@hotmail.com>
2024-01-02 09:36:48 +0000

    set real (not as simulation) auto renewal of certbot encryption key

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,6 +4,6 @@ COMPOSE="/usr/bin/docker-compose --no-ansi "
 DOCKER="/usr/bin/docker"
 
 cd /home/insoo223_gmail_com/github/node_project/
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
 
